4ba72cfadea80b0bd03efc77b4bc71a041d1279c
Kate <kate.dovzhenk@gmail.com>
2025-06-05 10:18:52 -0400

    Schema was updated

--- src/db/models/session.js
+++ src/db/models/session.js
@@ -1,29 +1,30 @@
 import mongoose from 'mongoose';
 
-const sessionSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
+const sessionSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    accessToken: {
+      type: String,
+      required: true,
+    },
+    refreshToken: {
+      type: String,
+      required: true,
+    },
+    accessTokenValidUntil: {
+      type: Date,
+      required: true,
+    },
+    refreshTokenValidUntil: {
+      type: Date,
+      required: true,
+    },
   },
-  accessToken: {
-    type: String,
-    required: true,
-  },
-  refreshToken: {
-    typr: String,
-    required: true,
-  },
-  accessTokenValidUntil: {
-    type: Date,
-    required: true,
-  },
-  refreshTokenValidUntil: {
-    type: Date,
-    required: true,
-  },
-  timestamps: true,
-  versionKey: false,
-});
+  { timestamps: true, versionKey: false },
+);
 
 const Session = mongoose.model('Session', sessionSchema);
 
