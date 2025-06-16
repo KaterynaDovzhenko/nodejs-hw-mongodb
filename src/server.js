f80c43c93ece7d0c06aa3220a873893bed5b0c19
Kate <kate.dovzhenk@gmail.com>
2025-06-16 16:35:23 -0400

    documentation was added

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
+import * as fs from 'node:fs';
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import swaggerUI from 'swagger-ui-express';
 
 import router from './routes/index.js';
 import path from 'node:path';
@@ -11,11 +13,19 @@ import { getEnvVar } from './utils/getEnvVar.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
+const SWAGGER_DOCUMENT = JSON.parse(
+  fs.readFileSync(path.join('docs', 'swagger.json'), 'utf-8'),
+);
+
 dotenv.config();
 const PORT = Number(getEnvVar('PORT', '7070'));
 
 export function setupServer() {
   const app = express();
+
+  if (process.env.NODE_ENV === 'development') {
+    app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(SWAGGER_DOCUMENT));
+  }
 
   app.use('/photos', express.static(path.resolve('src', 'uploads', 'photos')));
 
